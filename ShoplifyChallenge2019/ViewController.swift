54b6977cfd3f4db2dda2128dcbb224845e14e054
Jackson Ho <mintri1199@gmail.com>
2019-04-01 16:25:05 -0700

    Initial Commit

---
+++ ShoplifyChallenge2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoplifyChallenge2019
+//
+//  Created by Jackson Ho on 4/1/19.
+//  Copyright © 2019 Jackson Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
